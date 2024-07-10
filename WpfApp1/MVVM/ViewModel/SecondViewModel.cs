8370b82ba530157d8447326e226bac66d94808f2
wyrdnixx <jojo@ulewu.de>
2024-07-10 07:13:53 +0200

    Projektdateien hinzufügen.

---
+++ WpfApp1/MVVM/ViewModel/SecondViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KistPack_Wpf.MVVM.ViewModel
+{
+    class SecondViewModel
+    {
+    }
+}
